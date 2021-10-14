dda44069bb86fdf8d1ce726a2b50bee264a121c8
Amit Porje <52151309+amit4j@users.noreply.github.com>
2021-10-14 00:16:18 -0400

    Setup basic flask app

---
+++ service/__init__.py
@@ -0,0 +1,38 @@
+"""
+Package: service
+Package for the application models and service routes
+This module creates and configures the Flask app and sets up the logging
+and SQL database
+"""
+import logging
+import os
+
+from flask import Flask
+
+# Create Flask application
+app = Flask(__name__)
+app.config.from_object("config")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+PORT = os.getenv("PORT", "5000")
+
+# Import the rutes After the Flask app is created
+from service import routes, models, error_handlers
+
+# Set up logging for production
+print("Setting up logging for {}...".format(__name__))
+app.logger.propagate = False
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    # Make all log formats consistent
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
+    )
+    for handler in app.logger.handlers:
+        handler.setFormatter(formatter)
+    app.logger.info("Logging handler established")
+
+app.logger.info(70 * "*")
+app.logger.info("=====   RECOMMENDATION SERVICE   =====".center(70, "*"))
+app.logger.info(70 * "*")
