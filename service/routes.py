c52019ca3686f2899ec27a594e5cfdaafb087290
Jeff Jiang <jefferyjee@gmail.com>
2021-10-14 22:56:35 -0400

    added list_recommendations API

--- service/routes.py
+++ service/routes.py
@@ -19,3 +19,15 @@ def index():
     """Root URL response"""
     app.logger.info("Request for Root URL")
     return (jsonify(), status.HTTP_200_OK)
+
+@app.route("/recommendations/<int:prod_id>", methods=["GET"])
+def list_recommendations(prod_id):
+    """Returns all of recommendations for a product"""
+    app.logger.info("Request for recommendations for product with id: %s", prod_id)
+    product = Product.find(prod_id)
+    if not product:
+        raise NotFound("Product with id '{}' was not found.".format(prod_id))
+
+    app.logger.info("Returning recommendations for product: %s", product.name)
+    return make_response(jsonify(product.serialize()), status.HTTP_200_OK)
+    
