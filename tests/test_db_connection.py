1b78289121dcea08bf57a382fdf301be588ca586
Amit Porje <52151309+amit4j@users.noreply.github.com>
2021-10-15 17:09:46 -0400

    Revert "Update test_db_connection.py"
    This reverts commit edb98c46fbc460e86451b8bf3b26bf650f010a3b.

--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -5,32 +5,29 @@ Test cases can be run with:
     coverage report -m
 While debugging just these tests it's convinient to use this:
     nosetests --stop tests/test_pets.py:TestPetModel
-# """
-# import os
-# import logging
-# import unittest
-# from werkzeug.exceptions import NotFound
-# from service import app
+"""
+import os
+import logging
+import unittest
+from werkzeug.exceptions import NotFound
+from service import app
 
-from flask.ext.testing import TestCase
-from myapp import create_app, db
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/testdb"
+)
 
 ######################################################################
 #  T E S T   D A T A   B A S E  
 ######################################################################
 
-class MyTest(TestCase):
+class TestPetModel(unittest.TestCase):
+    """Test Cases for Pet Model"""
 
-    DATABASE_URI = os.getenv("DATABASE_URI", "postgres://postgres:postgres@localhost:5432/testdb")
-    TESTING = True
-
-    def create_app(self):
-        # pass in test configuration
-        return create_app(self)
-
-    def setUp(self):
-        db.create_all()
-        
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+    @classmethod
+    def setUpClass(cls):
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        pass
