effb9128f6733cd0155a1e763e2d816042f4c3b4
Jenny <90348925+JennyHWAN@users.noreply.github.com>
2021-10-15 23:35:22 -0400

    Feature/root url (#19)
    * Added a name version description and path in index
    
    * Added test for index URL

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,27 +18,35 @@ class TestYourResourceServer(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
-        pass
+        """Run once before all tests"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        # Set up the test database
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
-        pass
+        """Run once after all tests"""
+        db.session.close()
 
     def setUp(self):
-        """ This runs before each test """
+        """Runs before each test"""
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # create new tables
         self.app = app.test_client()
 
     def tearDown(self):
-        """ This runs after each test """
-        pass
-
+        db.session.remove()
+        db.drop_all()
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
 
     def test_index(self):
-        """ Test index call """
+        """Test the Home Page"""
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], "Recommendation REST API Service")
