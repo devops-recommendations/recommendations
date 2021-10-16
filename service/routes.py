bfab91a9246b135b02a9fc593362fbdb709b2ade
Jenny <90348925+JennyHWAN@users.noreply.github.com>
2021-10-15 22:48:46 -0400

    Added a name version description and path in index

--- service/routes.py
+++ service/routes.py
@@ -13,9 +13,19 @@ from . import app
 from . import status  # HTTP Status Codes
 
 
+######################################################################
 # GET INDEX
+######################################################################
 @app.route("/")
 def index():
     """Root URL response"""
     app.logger.info("Request for Root URL")
-    return (jsonify(), status.HTTP_200_OK)
+    return (
+        jsonify(
+            name="Recommendation REST API Service",
+            version="1.0",
+            description="The recommendations resource can be used to get a product recommendation based on another product.",
+            paths=[url_for("list_recommendations", _external=True)],
+        ),
+        status.HTTP_200_OK,
+    )
