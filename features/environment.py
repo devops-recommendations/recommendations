6068c0c0915811d00f7ed813f276edc5f70d79cd
Amit Porje <52151309+amit4j@users.noreply.github.com>
2021-12-01 18:24:23 -0500

    Change behave test url to dev

--- features/environment.py
+++ features/environment.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 
 WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
 BASE_URL = getenv(
-    'BASE_URL', 'http://localhost:5000')
+    'BASE_URL', 'https://nyu-recommendations-service-fall2021.us-south.cf.appdomain.cloud')
 
 
 def before_all(context):
